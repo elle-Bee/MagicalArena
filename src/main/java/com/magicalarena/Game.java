66066e153bc5b6e609b5f45138bfde24602b6eba
elle-Bee <65856801+elle-Bee@users.noreply.github.com>
2024-07-09 00:36:25 +0530

    Added Game class with attributes and basic methods

---
+++ src/main/java/com/magicalarena/Game.java
@@ -0,0 +1,13 @@
+package com.magicalarena;
+
+public class Game {
+    private Arena arena;
+
+    public Game (Arena arena){
+        ths.arena = arena
+    }
+
+    public void play() {
+
+    }
+}
